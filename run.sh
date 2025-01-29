77218a1f0c4a7f8673566f8c59322390a2487df6
vyasakhilesh <akh.vyas@gmail.com>
2025-01-29 11:44:24 +0100

    added docker compose files

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker compose up -d --build
 
+# curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.10.4/docker-compose.yaml'
+# docker compose up -d --
+docker compose -f docker-compose_db.yaml -f docker-compose_spark.yaml -f docker-compose_airflow.yaml up --build
