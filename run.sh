f1e95bbfda312be1ef0b307108c72335d9a350d9
vyasakhilesh <akh.vyas@gmail.com>
2025-02-07 18:24:42 +0100

    airbyte configuration

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@
 docker compose -f docker-compose_db.yaml up -d --build --force-recreate
 docker compose -f docker-compose_spark.yaml up -d --build --force-recreate --scale spark-worker=3
 docker compose -f docker-compose_airflow.yaml up -d --build --force-recreate
+docker compose -f docker-compose_airbyte.yaml up -d --build --force-recreate
 docker compose -f docker-compose_qdrant.yaml up -d --build --force-recreate
 # mongodb://root:example@localhost:27017/
 
@@ -18,3 +19,6 @@ docker compose -f docker-compose_db.yaml -f docker-compose_spark.yaml -f docker-
 docker exec spark-master-container spark-submit --packages io.delta:delta-spark_2.12:3.3.0 --master spark://spark-master:7077 --deploy-mode client ./apps/$(app)
 # echo "172.22.0.2 spark-master" >> /etc/hosts
 docker exec spark-master-con spark-submit --packages io.delta:delta-spark_2.12:3.3.0 --master spark://spark-master:7077 --deploy-mode client ./scripts/process_json_to_delta.py
+
+# Airbyte
+ip addr show eth0 | grep inet | awk '{ print $2 }' | cut -d/ -f1
